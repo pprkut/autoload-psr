070a43b9a7d4d2cfbc82663a6c3707c8286f03e3
Heinz Wiesinger <pprkut@liwjatan.org>
2025-06-30 16:16:00 +0200

    Add a stub file for IDEs

---
+++ autoload_psr.stub.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * PHPDoc stub file for autoload_psr extension
+ * @author Heinz Wiesinger <pprkut@liwjatan.org>
+ */
+
+/**
+ * Register a namespace prefix for PSR-4.
+ *
+ * @param string prefix PSR-4 prefix
+ * @param string path   Path to the PHP classes for the prefix
+ *
+ * @return void
+ **/
+function autoload_register_psr4_prefix(string prefix, string path) : void
+{
+}
+
+?>
